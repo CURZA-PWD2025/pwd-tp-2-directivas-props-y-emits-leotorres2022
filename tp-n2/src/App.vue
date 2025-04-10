71b800476abd376c66f63b9568a8d0d1bddf73b0
leotorres <leo_escobar11@hotmail.com>
2025-04-10 17:29:47 -0300

    version final

--- tp-n2/src/App.vue
+++ tp-n2/src/App.vue
@@ -1,6 +1,8 @@
-<template>
-    <CardComponent v-for="list in peli" :movie="list" />
-
+<template >
+  <div class="listado"> 
+    <CardComponent v-for="list in peli" :movie="list" @update-likes="handleUpdateLikes(list.id)" />
+    </div >  
+    
 
 </template>
 
@@ -10,9 +12,17 @@ import peliculas from './resource/movies';
 import { reactive } from 'vue';
 import CardComponent from './components/CardComponent.vue';
 
-let peli : Array <Pelicula> = reactive (peliculas)
-
+const peli: Array<Pelicula> = reactive(peliculas);
+   
+    const handleUpdateLikes = (index: number) => (value: number) => {
+    peli[index].likes += value }
 </script>
 <style scoped>
+.listado {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); 
+    gap: 10px; 
+    padding: 10px; 
+}
 
 </style>
--- tp-n2/src/components/CardComponent.vue
+++ tp-n2/src/components/CardComponent.vue
@@ -1,28 +1,54 @@
 <template>
-    <div>
+    <div class="peliculas">
 
 <h2>Id:{{ props.movie.id }}</h2>
 <h2>Titulo:{{ props.movie.titulo}}</h2>
-<h2>Año:{{ props.movie.anio }}</h2>
-<h2>Generos:{{ props.movie.generos }}</h2>
-<h2>Director:{{ props.movie.director}}</h2>
-<img :src="props.movie.portada" alt="">  
-<br> 
-<button @click="" color="blue">Likes:{{ props.movie.likes}}</button>
+<h3>Año:{{ props.movie.anio }}</h3>
+<h3>Generos:{{ props.movie.generos }}</h3>
 
+<h3>Director :{{ props.movie.director}}</h3>
+<img :src="props.movie.portada" alt="" height="500" width="300" />  
+<br> 
+<button @click="DarLike" color="blue">
+             {{ props.movie.likes }}
+        </button>
       </div>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from 'vue';
 import type { Pelicula } from '../interfaces/Pelicula';
 const props = defineProps <{movie:Pelicula}>()
+const emit = defineEmits(['update-likes']);
+const isLiked = ref(false); 
 
+
+const DarLike= () => {
+    isLiked.value = !isLiked.value;
+    if (isLiked.value) {
+        props.movie.likes++;
+    } else {
+        props.movie.likes--;
+    }
+    emit('update-likes', props.movie.likes);
+};
 
 
 
 </script>
 
 <style scoped>
-
+.peliculas {
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+.peliculas h2 {
+    color: #333;
+    font-size: 1.5em;
+    margin: 5px 0;
+}
 </style>
